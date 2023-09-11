e0fb6df95883f3691a424f03a5f285f03f6492bb
raubertldlc <r.aubert@ldlc.com>
2023-09-11 16:30:40 +0200

    edit

--- src/components/TapInGenerator.vue
+++ src/components/TapInGenerator.vue
@@ -26,7 +26,7 @@
       <div class="separator-medium"> </div>
     <BoxTapin @close-popup="closePopup()" :contentDisplay="contentDisplay" :imgDisplay="imgDisplay" :twitterShareUrl="twitterShareUrl" />
     <PessiText @close-popup="closePopup()" :contentDisplay="contentDisplayPessi"  :twitterShareUrl="twitterShareUrlPessiText"/>
-    <SubGoal count="358"/>
+    <SubGoal count="396"/>
     <FooterComponent :isGenerating="isGenerating"/>
   </div>
 </template>
