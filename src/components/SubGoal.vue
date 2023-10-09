26b7cdbb331f52558eaa79756390eabb3e44dea7
ReZoKi <47327829+Resoki@users.noreply.github.com>
2023-10-09 14:50:42 +0200

    Update SubGoal.vue

--- src/components/SubGoal.vue
+++ src/components/SubGoal.vue
@@ -19,7 +19,7 @@ export default {
   },
   computed: {
     progressStyle() {
-      const percentage = (this.count / this.objective) * 100;
+      const percentage = (this.count / 1000) * 100;
       return `${percentage}%`;
     },
   },
